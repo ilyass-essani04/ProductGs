0c5c30b7b845bcdcb54b32b953f207683e9e1e7a
ilyass-essani04 <essani2004ilyas@gmail.com>
2024-03-27 15:37:46 +0000

    ferst

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>product</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="loginTitre">Login</h1>
+        <form action="product.php" method="post" class="form" >
+            <input type="text" name="" id="" placeholder='login' class="inputs">
+            <input type="password" name="" id="" placeholder='password' class="inputs">
+            <button class="inputs" type="submit">Se Connecter</button> 
+        </form>
+    </div>
+    
+</body>
+</html>
